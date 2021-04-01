4752f2df7765182c502a6454d2819f68031074e6
wangyibing <wangyibing36@gmail.com>
2021-04-01 14:46:45 +0800

    refactor

--- dubbo-hmily-demo/dubbo-account/src/main/java/io/dobson/account/service/impl/AccountServiceImpl.java
+++ dubbo-hmily-demo/dubbo-account/src/main/java/io/dobson/account/service/impl/AccountServiceImpl.java
@@ -3,20 +3,32 @@ package io.dobson.account.service.impl;
 import io.dobson.account.entity.AccountInfo;
 import io.dobson.account.mapper.AccountInfoMapper;
 import io.dobson.account.service.AccountService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.dromara.hmily.annotation.HmilyTCC;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 @Service
 @DubboService(version = "1.0.0")
+@Slf4j
 public class AccountServiceImpl implements AccountService {
 
     @Resource
     private AccountInfoMapper accountInfoMapper;
 
     @Override
+    @HmilyTCC(confirmMethod = "confirmMethod", cancelMethod = "cancelMethod")
     public boolean pay(AccountInfo accountInfo) {
         return accountInfoMapper.exchange(accountInfo);
+    }
+
+    public void confirmMethod (){
+        log.info("confirm 交易操作================");
+    }
+
+    public void cancelMethod(){
+        log.info("cancel 交易操作==================");
     }
 }
